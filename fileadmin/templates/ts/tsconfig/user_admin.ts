141dfec568be1f30b4afbf16ab745c08d952ce0f
Niels Tiedt <cool_coders@web.de>
2013-07-23 16:28:39 +0200

    TSCONFIG
    - Added admin TSConfig to show page id before page title in page view

--- fileadmin/templates/ts/tsconfig/user_admin.ts
+++ fileadmin/templates/ts/tsconfig/user_admin.ts
@@ -1,2 +1,7 @@
 # Schaltet das cachen von externen TS-Dateien aus
 admPanel.override.tsdebug.forceTemplateParsing = 1
+
+# Zeigt im Seitenbaum vor dem Seitennamen die ID der Seite
+options { 
+  pageTree.showPageIdWithTitle = 1
+}
