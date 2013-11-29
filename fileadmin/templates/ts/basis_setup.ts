6a955e7c6638cae978fb234827c60c4ca66d9892
Niels Tiedt <cool_coders@web.de>
2013-11-29 10:25:38 +0100

    Moved TS file includes to an extra file

--- fileadmin/templates/ts/basis_setup.ts
+++ fileadmin/templates/ts/basis_setup.ts
@@ -438,30 +438,5 @@ page.10000{
 
 
 #----------------------------------------------------------- EXTERNAL-TS-SCRIPT-FILE-INCLUDE-BEGIN
-### css_styled_content Definitionen ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/css_styled_content.ts">
-
-### css_styled_content Definitionen ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/t3d_debug.ts">
-
-### Seitentypen ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/t3d_seitentypen.ts">
-
-### Funktionen ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/funktionen_setup.ts">
-
-### CSS ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/css_setup.ts">
-
-### JS ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/js_setup.ts">
-
-### Navigation ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/navigation_setup.ts">
-
-### Objekte ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/objekte_setup.ts">
-
-### E-Tracker ###
-<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/t3d_webanalytic.ts">
+<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/t3d_includes.ts">
 #----------------------------------------------------------- EXTERNAL-TS-SCRIPT-FILE-INCLUDE-END
