8926359d91c82c860a176d70aaf4c97f0e816ae3
Niels Tiedt <cool_coders@web.de>
2014-05-27 16:34:49 +0200

    Added background-color in list view TYPO3 > 6

--- fileadmin/templates/ts/tsconfig/user_admin.ts
+++ fileadmin/templates/ts/tsconfig/user_admin.ts
@@ -1,6 +1,9 @@
 # Include following tag without # in the user TSconfig section of your admin user
 #<INCLUDE_TYPOSCRIPT:source="file:fileadmin/templates/ts/tsconfig/user_admin.ts">
 
+# BE background-color in list view
+#options.pageTree.backgroundColor.1 = red
+
 # Schaltet das cachen von externen TS-Dateien aus
 admPanel.override.tsdebug.forceTemplateParsing = 1
 
