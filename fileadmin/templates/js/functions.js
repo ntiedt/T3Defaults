ba42fc41a140e1e656ae3be2ab578ca621568f96
Niels Tiedt <nt@typo3-coder.org>
2015-05-07 10:49:52 +0200

    - Switched to $ instead of j because of simplification - Fixed false simple if using

--- fileadmin/templates/js/functions.js
+++ fileadmin/templates/js/functions.js
@@ -7,7 +7,7 @@ function dbg(el) {
     }
 }
 
-jQuery(function (j) {
+jQuery(function ($) {
     var the_overlay;
     if (typeof isTouch != 'undefined') {
         if (isTouch == 1) {
@@ -15,10 +15,10 @@ jQuery(function (j) {
         } else {
             isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
         }
-        if (isTouch == true) j('body').addClass('touch');
+        if (isTouch == true) $('body').addClass('touch');
     }
 
-    j(document).keydown(function (e) {
+    $(document).keydown(function (e) {
         // Shift / Strg / Alt / D
         if (e.which === 68 && e.ctrlKey && e.altKey && e.shiftKey) {
             if (debug == 1) {
@@ -32,44 +32,43 @@ jQuery(function (j) {
 
     /* SETUP: Ajax */
     if (typeof ajax_param != 'undefined') {
-        j.ajaxSetup({
+        $.ajaxSetup({
             data: {'type': ajax_param}
         });
     }
 
     /* EVENTS (nav_main / nav_header) */
-    j('.nav_main ul.level-1 > li').add('.nav_header ul.level-1 > li').hover(
+    $('.nav_main ul.level-1 > li').add('.nav_header ul.level-1 > li').hover(
         function () {
-            j(this).addClass('hover');
-            j(this).find('ul.level-2').position({
-                of: j(this).find('a.level-1'),
+            $(this).addClass('hover');
+            $(this).find('ul.level-2').position({
+                of: $(this).find('a.level-1'),
                 my: 'left top',
                 at: 'left bottom',
                 collision: 'flipfit'
             });
         }, function () {
-            j(this).removeClass('hover');
+            $(this).removeClass('hover');
         }
     );
 
     /* FUNC: Jobs: Uebergabe */
-    if (j('* a.internal-job').length > 0) {
-        href = j('* a.internal-job').attr('href');
+    if ($('* a.internal-job').length > 0) {
+        href = $('* a.internal-job').attr('href');
         field = '&job=';
-        job = j('.news-single-item h1:first').text();
-        j('* a.internal-job').attr('href', encodeURI(href + field + job));
-        j('form.powermail_form .powermail_field:first').val(job);
+        job = $('.news-single-item h1:first').text();
+        $('* a.internal-job').attr('href', encodeURI(href + field + job));
+        $('form.powermail_form .powermail_field:first').val(job);
     }
-    ;
 
     /* COLORBOX: http://www.jacklmoore.com/colorbox */
     if (typeof lightboxEnabled != 'undefined') {
         if (lightboxEnabled == 1) {
             if (jQuery().colorbox) {
-                j('a.lightbox').colorbox({
+                $('a.lightbox').colorbox({
                     onOpen: function () {
-                        if (j('.big_title').length < 1) {
-                            j('#cboxTopCenter').append(big_title + logo);
+                        if ($('.big_title').length < 1) {
+                            $('#cboxTopCenter').append(big_title + logo);
                         }
                     },
                     'maxWidth': maxWidth,
@@ -84,12 +83,12 @@ jQuery(function (j) {
     }
 
     /* Event Tracker */
-    etracker_eventtracker = (typeof etracker_eventtracker == 'undefined') ? etracker_eventtracker = '' : etracker_eventtracker = etracker_eventtracker;
-    google_analytics_eventtracker = (typeof google_analytics_eventtracker == 'undefined') ? google_analytics_eventtracker = '' : google_analytics_eventtracker = google_analytics_eventtracker;
+    etracker_eventtracker = (typeof etracker_eventtracker == 'undefined') ? '' : etracker_eventtracker;
+    google_analytics_eventtracker = (typeof google_analytics_eventtracker == 'undefined') ? '' : google_analytics_eventtracker;
     if ((etracker_eventtracker == 1) || (google_analytics_eventtracker == 1)) {
-        j.debug(etracker_eventtracker);
-        j.debug(google_analytics_eventtracker);
-        j('body a').t3d_webanalytic_event_tracking({
+        $.debug(etracker_eventtracker);
+        $.debug(google_analytics_eventtracker);
+        $('body a').t3d_webanalytic_event_tracking({
             config_baseurl: config_baseurl,
             etracker_eventtracker: etracker_eventtracker,
             google_analytics_eventtracker: google_analytics_eventtracker,
@@ -99,7 +98,7 @@ jQuery(function (j) {
 
     /* FLEXSLIDER: */
     if (jQuery().flexslider) {
-        j('.flexslider').flexslider({
+        $('.flexslider').flexslider({
             animation: "slide",
             selector: ".slides > .csc-textpic-imagerow",
             slideshowSpeed: 7000,
